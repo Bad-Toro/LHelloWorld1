df44cdb8dd3add71851a3dab70fe413b568edfb7
ferna <fernando.toro@mobileappscompany.com>
2017-03-29 16:10:07 -0400

    +Calculator

--- app/src/main/java/com/mobileappscompany/training/helloworld1/MainActivity.java
+++ app/src/main/java/com/mobileappscompany/training/helloworld1/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Com
+        String s = getString(R.string.thisString);
+
     }
 }
